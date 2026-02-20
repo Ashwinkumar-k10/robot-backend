4ba4cc1372d3f09b25594b4cb743f3fe26123b8c
Ashwinkumar <ashwinkumartry@gmail.com>
2026-02-20 06:40:21 +0530

    ready for cyclic deploy

--- server.js
+++ server.js
@@ -7,29 +7,28 @@ app.use(cors());
 
 let latestMessage = "No Data Yet";
 
-// MQTT connection
+// MQTT
 const client = mqtt.connect("mqtt://broker.hivemq.com:1883");
 
 client.on("connect", () => {
-  console.log("✅ Connected to MQTT Broker");
+  console.log("Connected to MQTT");
   client.subscribe("esp32/data");
 });
 
 client.on("message", (topic, message) => {
   latestMessage = message.toString();
-  console.log("📥 Received:", latestMessage);
+  console.log("Received:", latestMessage);
 });
 
 app.get("/", (req, res) => {
-  res.send("🚀 Robot Backend Running");
+  res.send("Robot Backend Running");
 });
 
 app.get("/data", (req, res) => {
   res.json({ data: latestMessage });
 });
 
-// IMPORTANT FOR RAILWAY
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`🌐 Server running on port ${PORT}`);
+  console.log("Server running on port", PORT);
 });
